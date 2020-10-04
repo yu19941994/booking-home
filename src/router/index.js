62d06bc4ad97e08b873a2a3e555ceaad10899e3c
yishian yu <yishian.yu@gmail.com>
2020-10-04 19:58:24 +0800

    1004

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import RoomIntroduce from '../views/RoomIntroduce.vue'
+// import VCalendar from 'v-calendar'
 
 Vue.use(VueRouter)
+// Vue.use(VCalendar)
 
 const routes = [
   {
@@ -11,12 +14,9 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/roomintroduce/:id',
+    name: 'RoomIntroduce',
+    component: RoomIntroduce
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div class="swiper">
       <!-- 底層Swipper -->
-      <swiper ref="mySwiper" :options="swiperOptions" class="swiper-container">
+      <swiper ref="mySwiper" :options="swiperOption" class="swiper-container">
         <swiper-slide class="slide"><img src="@/assets/image/bg1.jpeg" alt="" class="bg"></swiper-slide>
         <swiper-slide class="slide"><img src="@/assets/image/bg2.jpeg" alt="" class="bg"></swiper-slide>
         <swiper-slide class="slide"><img src="@/assets/image/bg3.jpeg" alt="" class="bg"></swiper-slide>
@@ -15,40 +15,55 @@
       </div>
 
       <div class="container">
-       <div class="container-inside">
+        <div class="container-inside">
           <!-- left container -->
           <div class="left-container">
             <div class="logo">
               <img src="@/assets/image/logo.svg" alt="">
             </div>
             <div class="title-text">
-              <p class="font font-h5 font-light">好室旅店。HOUSE HOTEL</p>
-              <p class="font-h5 font-light">花蓮縣花蓮市國聯一路1號</p>
-              <p class="font-h5 font-light">03-8321155</p>
-              <p class="font-h5 font-light">HOUSE@HOTEL.COM</p>
+              <p class="font font-h5 fontC-light">好室旅店。HOUSE HOTEL</p>
+              <p class="font-h5 fontC-light">花蓮縣花蓮市國聯一路1號</p>
+              <p class="font-h5 fontC-light">03-8321155</p>
+              <p class="font-h5 fontC-light">HOUSE@HOTEL.COM</p>
             </div>
           </div>
 
-          <div class="grid-button">
-            <div><img src="@/assets/image/room1A.jpeg" alt="" class="room"></div>
-            <div><img src="@/assets/image/room2A.jpeg" alt="" class="room"></div>
-            <div><img src="@/assets/image/room3A.jpeg" alt="" class="room"></div>
-            <div><img src="@/assets/image/room4A.jpeg" alt="" class="room"></div>
-            <div><img src="@/assets/image/room5A.jpeg" alt="" class="room"></div>
-            <div><img src="@/assets/image/room6A.jpeg" alt="" class="room"></div>
+          <div class="grid-container">
+            <div class="grid-button">
+              <div class="room1">
+                <img src="@/assets/image/room1A.jpeg" alt="" class="room">
+              </div>
+              <div class="room2">
+                <img src="@/assets/image/room2A.jpeg" alt="" class="room">
+              </div>
+              <div class="room3">
+                <img src="@/assets/image/room3A.jpeg" alt="" class="room">
+              </div>
+              <div class="room4">
+                <img src="@/assets/image/room4A.jpeg" alt="" class="room">
+              </div>
+              <div class="room5">
+                <img src="@/assets/image/room5A.jpeg" alt="" class="room">
+              </div>
+              <div class="room6">
+                <img src="@/assets/image/room6A.jpeg" alt="" class="room">
+              </div>
+            </div>
+            <div class="grid-layer">
+              <router-link class="grid-text" v-for="item in room" :key="item.id" :to="`roomintroduce/${item.id}`">
+                <p class="font font-h4 fontC-light text">{{item.name}}</p>
+              </router-link>
+            </div>
           </div>
-       </div>
+        </div>
       </div>
-      
-      
     </div>
-  
   </div>
 </template>
 
-<script> 
-// import { Carousel, Slide } from 'vue-carousel'
-// import $ from 'jquery'
+<script>
+
 import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
 import 'swiper/swiper-bundle.css'
 
@@ -57,29 +72,45 @@ export default {
   directives: {
     swiper: directive
   },
-  data (){
+  data () {
     return {
-      swiperOptions: {
-        // spaceBetween: 30,
+      room: [],
+      swiperOption: {
         pagination: {
-          el: '.swiper-pagination',
-          clickable: true
-        },
+          el: '.swiper-pagination'
+        }
       },
-      imgList:[
+      imgList: [
         {
-          name: "@/assets/image/bg1.jpeg"
+          name: '@/assets/image/bg1.jpeg'
         },
         {
-          name: "@/assets/image/bg2.jpeg"
+          name: '@/assets/image/bg2.jpeg'
         },
         {
-          name: "@/assets/image/bg3.jpeg"
+          name: '@/assets/image/bg3.jpeg'
         },
         {
-          name: "@/assets/image/bg4.jpeg"
+          name: '@/assets/image/bg4.jpeg'
         }
       ]
+    }
+  },
+  methods: {
+    getRoom () {
+      const vm = this
+      this.axios({
+        method: 'get',
+        baseURL: 'https://challenge.thef2e.com/api/thef2e2019/stage6/rooms',
+        headers: {
+          Authorization: 'Bearer ZpNjg6fR488qhR6rBqkJUPUxciEgE5JfKKuvTBSEa8WyQu5ifFDykP9yVJQt',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then((response) => {
+          vm.room = response.data.items
+          console.log(response.data.items)
+        })
     }
   },
   computed: {
@@ -89,7 +120,11 @@ export default {
   },
   mounted () {
     console.log('Current Swiper instance object', this.swiper)
-    // this.swiper.slideTo(3, 20000, true)
+    this.swiper.slideTo(3, 20000, true)
+    this.getRoom()
+  },
+  created () {
+    // this.getRoom()
   }
 }
 </script>
